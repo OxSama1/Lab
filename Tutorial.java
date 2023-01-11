4531a031aa10ae4d1be5b24da5d5a76639ce21db
OxSama1 <122457385+OxSama1@users.noreply.github.com>
2023-01-11 15:44:39 +0200

    files added
    the tutorial for java

---
+++ Tutorial.java
@@ -0,0 +1,14 @@
+public class Tutorial
+
+{
+    public static void main(String [] args)
+    {
+        int num1  = 7;
+        int x  = 10;
+       
+        System.out.println(x>num1);
+        System.out.println(num1);
+
+
+    }
+}
